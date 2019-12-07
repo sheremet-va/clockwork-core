c9f52fa1ba806af267952c0934f4095dbf624755
Vladimir <foxzdavinci@gmail.com>
2019-12-07 18:47:32 +0300

    subscriptions service: remove debugging

--- src/services/subscriptions.js
+++ src/services/subscriptions.js
@@ -6,13 +6,10 @@ module.exports = ( core, modules ) => {
     new CronJob( '0 50 */1 * * *', modules.drops.send ).start(); // DROPS subscription
     new CronJob( '5 */2 * * * *', modules.esn.send ).start(); // ESN-NEWS and RUESO subscriptions
     new CronJob( '10 */2 * * * 6', merchants.start ).start(); // GOLDEN and LUXURY subscriptions
+    new CronJob( '10 */2 * * * 6', merchants.start ).start(); // GOLDEN and LUXURY subscriptions
     new CronJob( '15 */2 * * * *', modules.news.send ).start(); // NEWS subscription
     new CronJob( '20 */2 * * * *', modules.patch.send ).start(); // PATCH subscription
     new CronJob( '25 00 9 * * *', modules.pledges.send ).start(); // PLEDGES subscription
     new CronJob( '30 */3 * * * *', modules.status.send ).start(); // STATUS subscription
     new CronJob( '35 */10 * * * 1-2', modules.weekly.send ).start(); // WEEKLY subscription
-
-    // TESTS
-    // new CronJob( '*/5 * * * * *', drops.send ).start(); // DROPS subscription
-    // new CronJob( '*/15 * * * * *', modules.status.send ).start(); // STATUS subscription
 };
