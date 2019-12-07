2602c8259b995548084e2692eec1ca925fc049b2
Vladimir <foxzdavinci@gmail.com>
2019-12-07 18:31:26 +0300

    logger: fixed logger path

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,17 +1,17 @@
 const moment = require( 'moment' );
-// const fs = require( 'fs' );
+const fs = require( 'fs' );
 
 exports.log = ( content, type = 'log' ) => {
     const timestamp = `[${moment().format( 'YYYY-MM-DD HH:mm:ss' )}]:`;
-    // const day = `${moment().format( 'YYYY-MM-DD' )}`;
+    const day = `${moment().format( 'YYYY-MM-DD' )}`;
 
     const message = `${timestamp} (${type.toUpperCase()}) ${content}`;
 
-    /* fs.appendFile( `logs/${day}-logs.txt`, `${message}\n`, err => {
-        if ( err ) fs.writeFile( `logs/${day}-logs.txt`, `${message}\n`, error => {
+    fs.appendFile( `src/logs/${day}-logs.txt`, `${message}\n`, err => {
+        if ( err ) fs.writeFile( `src/logs/${day}-logs.txt`, `${message}\n`, error => {
             if ( error ) throw new Error( err );
         });
-    }); */
+    });
 
     console.log( message );
 };
