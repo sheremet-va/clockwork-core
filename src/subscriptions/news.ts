df49a259cbe484d24d8390f427be83fd605c5208
Vladimir <sheremet.va@icloud.com>
2020-07-22 21:30:36 +0300

    fix(news): condition updated (between now and 3 minutes)

--- src/subscriptions/news.ts
+++ src/subscriptions/news.ts
@@ -48,26 +48,21 @@ export default class CronNews extends News {
 
     send = async (): Promise<void> => {
         const url = 'http://files.elderscrollsonline.com/rss/en-us/eso-rss.xml';
-        // const oldNews = await this.core.info.get<NewsInfo>('news');
-        const published = await this.core.info.get<{ news?: string[] }>('published') || {};
-
-        if(!('news' in published) || !published.news) {
-            published.news = [];
-        }
 
         const { data } = await this.core.request(url);
 
         const $ = cheerio.load(data as string, { normalizeWhitespace: true, xmlMode: true });
 
+        const threeMinutes = moment().subtract(3, 'minutes');
+
         const news = $('item').filter((_, news) => {
             const $news = $(news);
 
             const date = $news.find('pubDate').text();
-            const link = $news.find('link').text().trim();
 
             return (
                 moment().isSame(date, 'day')
-                && !(published.news || []).includes(link)
+                && moment(date).isBetween(threeMinutes, moment())
             );
         }).get()[0];
 
@@ -78,8 +73,6 @@ export default class CronNews extends News {
         const $news = $(news);
 
         const link = $news.find('link').text().trim();
-
-        published.news.push(link);
 
         const russian = await this.translate(link);
 
@@ -104,7 +97,6 @@ export default class CronNews extends News {
         };
 
         await this.core.info.set('news', description);
-        await this.core.info.set('published', published);
 
         const translations = this.core.translations.get('commands', 'news');
 
